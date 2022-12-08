7e7ba6caa3d2dea2ea2962f98e482e1d62401b86
MIGUEL ALMEYDA RAMOS <58181314+malmeydar@users.noreply.github.com>
2022-12-07 23:33:23 -0500

    Creando el Modelo Contacto al ApplicationDbContext

---
+++ SlnCrudASPnet7MVC/SlnCrudASPnet7MVC/Datos/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using SlnCrudASPnet7MVC.Models;
+
+namespace SlnCrudASPnet7MVC.Datos
+{
+
+   public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options) 
+        {
+
+        }
+        // Agregaremos los modelos
+        public DbSet<Contacto> Contacto { get; set; }  
+        
+    }
+}
---
+++ SlnCrudASPnet7MVC/SlnCrudASPnet7MVC/Models/Contacto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SlnCrudASPnet7MVC.Models
+{
+    public class Contacto
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="El nombre es obligatorio")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "El telefono es obligatorio")]
+        public string Telefono { get; set; }
+        [Required(ErrorMessage = "El movil es obligatorio")]
+        public string Movil { get; set; }
+        [Required(ErrorMessage = "El Email es obligatorio")]
+        public string Email { get; set; }
+        public DateTime FechaCreacion { get; set; }
+
+
+
+    }
+}

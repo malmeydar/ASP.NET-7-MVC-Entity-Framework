6f249a8fcbf6f48be119a3978260667ab1e9abc3
MIGUEL ALMEYDA RAMOS <58181314+malmeydar@users.noreply.github.com>
2022-12-10 13:03:20 -0500

    Eliminamos proyecto anterior para iniciar desde cero

--- SlnAppBlogCore/SlnAppBlogCore.Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SlnAppBlogCore.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}

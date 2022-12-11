4160e7b9cd0b6352891b667fab2555e9301a14e0
MIGUEL ALMEYDA RAMOS <58181314+malmeydar@users.noreply.github.com>
2022-12-10 21:10:41 -0500

    Se implemto el Repository Categorya y la interface ICategoria

---
+++ SlnAppBlogCoreProyect/SlnAppBlogCoreProyect.AccesoDatos/Data/Repository/CategoriaRepository.cs
@@ -0,0 +1,41 @@
+﻿using SlnAppBlogCoreProyect.AccesoDatos.Data.Repository.IRepository;
+using SlnAppBlogCoreProyect.Data;
+using SlnAppBlogCoreProyect.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace SlnAppBlogCoreProyect.AccesoDatos.Data.Repository
+{
+    public class CategoriaRepository : Repository<Categoria>, ICategoriaRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoriaRepository(ApplicationDbContext db): base(db) 
+        {
+            _db= db;
+        }
+
+        public IEnumerable<SelectListItem> GetListaCategorias()
+        {
+            return _db.Categoria.Select(i => new SelectListItem()
+            {
+                Text= i.Nombre,
+                Value = i.Id.ToString(),
+            }
+
+            );
+            
+        }
+
+        public void Update(Categoria categoria)
+        {
+            var objetoDesdeDB = _db.Categoria.FirstOrDefault(s => s.Id == categoria.Id);
+            objetoDesdeDB.Nombre= categoria.Nombre;
+            objetoDesdeDB.Orden=categoria.Orden;
+            _db.SaveChanges();
+        }
+    }
+}
